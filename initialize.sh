38c6ee60c457cbe3c31be553e23200fa1392b5ab
Joni Kukko <joni.kukko@outlook.com>
2015-03-18 14:52:55 +0200

    https, upstream??

--- initialize.sh
+++ initialize.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #
-# Clones the repositories using 'ssh'.
+# Clones the repositories using 'https'.
 
 # Clone our repositories.
-git clone git@github.com:N4SJAMK/teamboard-io.git           io
-git clone git@github.com:N4SJAMK/teamboard-api.git          api
-git clone git@github.com:N4SJAMK/teamboard-client.git       client
-git clone git@github.com:N4SJAMK/teamboard-client-react.git client-react
+git clone https://github.com/N4SJAMK/teamboard-io           io
+git clone https://github.com/N4SJAMK/teamboard-api          api
+git clone https://github.com/N4SJAMK/teamboard-client       client
+git clone https://github.com/N4SJAMK/teamboard-client-react client-react
