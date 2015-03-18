644af3c98b77169cef7c3c4c4419f02c2b201d90
Joni Kukko <joni.kukko@outlook.com>
2015-03-18 14:27:36 +0200

    test

---
+++ initialize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Clones the repositories using 'https'. Use the 'initialize-ssh' if you prefer
+# to use 'ssh' in cloning the repositories.
+
+# Clone our repositories.
+git clone https://github.com/N4SJAMK/teamboard-io           io
+git clone https://github.com/N4SJAMK/teamboard-api          api
+git clone https://github.com/N4SJAMK/teamboard-client       client
+git clone https://github.com/N4SJAMK/teamboard-client-react client-react
