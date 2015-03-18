4aadc1386ca60175c1a51af4a17c2ef9d0cff045
Joni Kukko <joni.kukko@outlook.com>
2015-03-18 14:47:00 +0200

    upstream??

--- initialize.sh
+++ initialize.sh
@@ -8,3 +8,4 @@ git clone https://github.com/N4SJAMK/teamboard-io           io
 git clone https://github.com/N4SJAMK/teamboard-api          api
 git clone https://github.com/N4SJAMK/teamboard-client       client
 git clone https://github.com/N4SJAMK/teamboard-client-react client-react
+
