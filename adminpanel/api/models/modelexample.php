e1564b32c546dc18c202714eb125243e2c177137
Huppupelikaani <huppupelikaani@hotmail.com>
2015-03-19 11:39:57 +0200

    api thingy

---
+++ adminpanel/api/models/modelexample.php
@@ -0,0 +1,31 @@
+<?php
+
+
+class user {
+	
+	private $_id;
+	private $_email;
+	private $_token;
+	
+	
+	function __construct ($params = array())
+	{
+		
+		$this->_id = isset($params['id']) ? $params['id'] : null;
+		$this->_email = isset($params['email']) ? $params['email'] : null;
+		$this->_token = isset($params['token']) ? $params['token'] : null;
+	
+	}
+	
+	
+	public function getId ()
+	{
+		return $this->_id;
+	}
+
+
+
+}
+
+
+$user = new user(array('id' => 1, 'email' => "matti@mieskolainen.fi"));
