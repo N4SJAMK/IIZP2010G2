96ef26d8c06c7bc6d63dc5997afa26ecac9186fa
Joni Kukko <joni.kukko@outlook.com>
2015-03-19 15:42:16 +0200

    Model example

--- adminpanel/api/model/user.class.php
+++ adminpanel/api/model/user.class.php
@@ -1,6 +1,9 @@
 <?php
 
+// Dummy example of model
 
+
+// model class always starts with model_ 
 class model_user {
 	
 	private $_id;
@@ -8,6 +11,8 @@ class model_user {
 	private $_token;
 	
 	
+    // constructor takes params-array that holds key/value-pairs for private variables.
+    // array key == instance variable _key
 	function __construct ($params = array())
 	{
 		
