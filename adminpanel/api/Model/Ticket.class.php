9784ffbce545729f4e1eec8cf648d025fda027f9
Valje <omaposti666@gmail.com>
2015-03-23 12:09:11 +0200

    boardId to board

--- adminpanel/api/Model/Ticket.class.php
+++ adminpanel/api/Model/Ticket.class.php
@@ -4,7 +4,7 @@ namespace api\Model;
 class Ticket extends \api\Model\BaseModel {
 	
 	public $id = null;
-	public $boardId = null;
+	public $board = null;
 	public $position = array('z' => NULL, 'y' => NULL, 'x' => NULL );
 	public $color = '#FFFFFF';
 	public $content = '';
