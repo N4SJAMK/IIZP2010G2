f35c6b789d4120ae983b18e08cbd481332e8acd9
Joni Kukko <joni.kukko@outlook.com>
2015-03-23 16:54:26 +0200

    foreach test for delete operations

--- adminpanel/index.php
+++ adminpanel/index.php
@@ -1,22 +1,62 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+
+date_default_timezone_set('Europe/Helsinki');
+header('Content-Type: text/html;charset=utf-8');
+mb_internal_encoding('UTF-8');
+
+
 
 // simple test for rest api
 
 
 
-echo '<h3>All users:</h3>';
-echo file_get_contents('http://localhost:8001/api/users');
-
-
-
-echo '<h3>All boards:</h3>';
-echo file_get_contents('http://localhost:8001/api/boards');
-
-
-
-echo '<h3>All tickets:</h3>';
-echo file_get_contents('http://localhost:8001/api/tickets');
-
-
+$users = json_decode(file_get_contents('http://localhost:8001/api/users'));
+echo '<h3>Users, boards and tickets</h3>';
+echo '<ul>';
+foreach ($users as $user) {
+    echo '
+        <li>
+            <form method="post" action="api/users/'.$user->_id.'">
+                <input type="hidden" name="REQUEST_METHOD" value="delete">
+                <input type="submit" value="Poista">
+                <span>'.$user->email.'</span>
+            </form>
+            <ul>
+        ';
+        foreach ($user->__boards as $board) {
+            echo '
+                <li>
+                    <form method="post" action="api/boards/'.$board->_id.'">
+                        <input type="hidden" name="REQUEST_METHOD" value="delete">
+                        <input type="submit" value="Poista">
+                        <span>'.(empty($board->name) ? '{null}' : $board->name).'</span>
+                    </form>
+                    <ul>
+                ';
+                    foreach ($board->__tickets as $ticket) {
+                        echo '
+                            <li>
+                            <form method="post" action="api/tickets/'.$ticket->_id.'">
+                                <input type="hidden" name="REQUEST_METHOD" value="delete">
+                                <input type="submit" value="Poista">
+                                <span>'.(empty($ticket->content) ? '{null}' : $ticket->content).'</span>
+                            </form>
+                            </li>
+                            ';
+                    }
+            echo '
+                    </ul>
+                </li>
+                ';
+        }
+    echo '
+            </ul>
+        </li>
+        ';
+}
+echo '</ul>';
 
 
