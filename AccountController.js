f802c466af21c2ba95a24912135899533aec1dc2
AatuPitkanen <aatu.pitkanen@gmail.com>
2015-03-28 23:36:22 +0200

    update

--- AccountController.js
+++ AccountController.js
@@ -1,68 +1,69 @@
 var app = angular.module('MainApp', ['ngRoute', 'ui.bootstrap']);
 
 app.controller('TicketController', [ '$http', '$scope', function($http, $scope) {
-     
-	    
-	    $scope.Lista = [];
-		$scope.GetUser = [];
-		$scope.GetBoard = [];
-		$scope.GetTicket = [];
-		$scope.GetArticle = [];
-		$scope.isCollapsed = false;
-		$scope.IdSelected = null;  //ng-model = "id" //ng-model = "tickets"
-		$scope.IdSelected2 = null;  
-	    $scope.RowClick = null;
-		
-		
-   
-           $http.get('http://127.0.0.1:8080/api/queue/5').success(function(data2) {
-		   $scope.GetUser= data2;
-		   console.log($scope.GetUser);
-		 //id = data.id;  //tickets = data.tickets; 
-			}); 
- 
- $scope.TicketCall = function (index, IdSelected, i, data, RowClick) {
-   
-    var data = data;
-    $scope.IdSelected = IdSelected;
-	$scope.RowClick = RowClick;
-	$http.get('http://127.0.0.1:8080/api/queue/5').success(function(data2) {
-		$scope.GetQueue = data2;  
-	
-		var url = data2[0].tickets;
-		var arrayLength = url.length;
-		console.log(url);
-		angular.forEach(url, function TicketCall(IdSelected) { //(var i = 0; i < arrayLength; ++i) //  for (i in url) {
-		$http.get('http://127.0.0.1:8080'+ IdSelected).success(function (data) {
-        
-		  $scope.GetBoard.push(data);
-		})	
-		 });  
-		 });	 
-	};
-	
-	$scope.ArticleCall = function (IdSelected2, RowClick) {
-	     
-		$http.get('http://127.0.0.1:8080/api/ticket/5').success(function(data) {
-		
-	    $scope.IdSelected2 = IdSelected2;
-		var url2 = data[0].articles;
-		//var arrayLength = url.length;
-		console.log(url2);
-		angular.forEach(url2, function ArticleCall(IdSelected2) { 
+         
+		   $scope.GetUser = [];
+		   $scope.GetTicket = [];
+		   $scope.GetBoard = [];
+		   $scope.Delete = [];
+		   $scope.isCollapsed = true;
+		   $scope.IdSelected = null;
+		   $scope.IdSelected2 = null;
+		   $scope.IdSelected3 = null;
+		   
 		 
-		$http.get('http://127.0.0.1:8080'+ IdSelected2).success(function (data3) {
-         $scope.GetArticle = data3;	
-         // $scope.Lista = data;	
-		  $scope.GetTicket.push(data3);
-		console.log(data3);
-	
-		})
-		 });
-		
-		 });
-	 	};	  		
+		   
+		 $scope.Update = function() { 
+           $http.get('http://localhost:8001/api/users').success(function(data) {
+		   $scope.GetUser = data;
+		  
+		    }); //http://127.0.0.1:8000/json*/
+				}
+	    $scope.BoardCall = function(IdSelected) {  
+		   $http.get('http://localhost:8001/api/users/' + IdSelected).success(function(data) {
+		   $scope.GetBoard = data;
+		   console.log(data);
+		   
+		    }); 
+			}
+		$scope.TicketCall = function(IdSelected2) {
+		   $http.get('http://localhost:8001/api/boards/' + IdSelected2).success(function(data) {
+		   $scope.GetTicket = data;
+		   console.log(IdSelected2);
+		   console.log(data);
+		    }); 	
+			}
+
+	$scope.deleteCall = function(IdSelected3) {
+    $http.delete('http://localhost:8001/api/users/' + IdSelected3)
+        .success(function(data) {
+            $scope.Delete = data;
+            console.log($scope.Delete);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+};	
+$scope.deleteBoard = function(IdSelected3) {
+    $http.delete('http://localhost:8001/api/boards/' + IdSelected3)
+        .success(function(data) {
+            $scope.Delete = data;
+            console.log($scope.Delete);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+};	
+$scope.deleteTicket = function(IdSelected3) {
+    $http.delete('http://localhost:8001/api/tickets/' + IdSelected3)
+        .success(function(data) {
+            $scope.Delete = data;
+            console.log($scope.Delete);
+        })
+        .error(function(data) {
+            console.log('Error: ' + data);
+        });
+};	
+
     }]);
-	
-	
 	
