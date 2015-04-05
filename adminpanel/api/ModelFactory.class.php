b8b71602793998dbdb5523ce94c7e038c98a892a
mikkoleppanen <tapsa@Tapani-MBP.mshome.net>
2015-04-05 17:08:37 +0300

    Mdelfactory validation
    Once again might be completely wrong. Making this is made harder by the
    fact that filter_var_array doesn’t support recursion.

--- adminpanel/api/ModelFactory.class.php
+++ adminpanel/api/ModelFactory.class.php
@@ -5,17 +5,46 @@ namespace api;
 
 final class ModelFactory
 {
-    
-    
-    
-    
+	//this is a test, modify values if need be
+    private $validatingArray = array(
+		'users' => array(
+			'_id' => FILTER_VALIDATE_INT,
+			'email' => FILTER_VALIDATE_EMAIL,
+			'password' => FILTER_SANITIZE_ENCODED,
+			'token' => FILTER_FLAG_NONE,
+			'__boards' => array(
+				'filter' => FILTER_VALIDATE_INT,
+				'flags' => FILTER_FORCE_ARRAY
+			)
+		),
+		'tickets' => array(
+			'_id' => FILTER_VALIDATE_INT,
+			'board' => FILTER_VALIDATE_INT,
+			'position' => array (
+				'filter' => FILTER_CALLBACK, 		//filter_var_array does not have recursion
+				'options' => validate_array(array(	//this is a workaround
+					'x' => FILTER_VALIDATE_INT,
+					'y' => FILTER_VALIDATE_INT,
+					'z' => FILTER_VALIDATE_INT
+				))
+			),
+			'color' => array( //chech if color format is #FFFFFF
+    			'filter' => FILTER_VALIDATE_REGEXP,
+    			'flags'     => FILTER_NULL_ON_FAILURE,
+    			'options'   => array('regexp'=>'/^#.[a-f0-9]{6}$/')
+  		  	),
+			'content' => FILTER_SANITIZE_ENCODED
+		)
+	);
     
     
     // $type is the name of mongo collection like users, boards, tickets, events
     public function createModel ($type, $array)
     {
+		
+		
         // array to stdClass object
-        $model = json_decode(json_encode($array));
+        $model = json_decode(json_encode(filter_var_array($array, $validatingArray)));
         
         
         // what is this shit??
@@ -29,12 +58,17 @@ final class ModelFactory
         if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
         
         
-        return $this->_validateModel($type, $model);
+        return $model;
         
     }
     
+	private function validate_array($args) {
+	    return function ($data) use ($args) {
+	        return filter_input_array($data, $args);
+	    };
+	}
     
-    
+    //not used at this moment
     private function _validateModel ($type, $model)
     {
         
