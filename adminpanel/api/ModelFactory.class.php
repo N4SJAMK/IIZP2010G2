957beddf94b4e51e74eccedd96b26adcf9e2afff
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-04-08 20:24:11 +0300

    Modelfactory changed back to original

--- adminpanel/api/ModelFactory.class.php
+++ adminpanel/api/ModelFactory.class.php
@@ -6,7 +6,7 @@ namespace api;
 final class ModelFactory
 {
 	//this is a test, modify values if need be
-    private $validatingArray = array(
+    /* private $validatingArray = array(
 		'users' => array(
 			'_id' => FILTER_SANITIZE_ENCODED,
 			'email' => FILTER_VALIDATE_EMAIL,
@@ -21,8 +21,8 @@ final class ModelFactory
 			'_id' => FILTER_SANITIZE_ENCODED,
 			'board' => FILTER_SANITIZE_ENCODED,
 			'position' => array (
-				'filter' => FILTER_CALLBACK, 		//filter_var_array does not have recursion
-				'options' => validate_array(array(	//this is a workaround
+				'filter' => FILTER_CALLBACK, 		//filter_var_array does not have recursion this is a workaround
+				'options' => validate_array(array(
 					'x' => FILTER_VALIDATE_INT,
 					'y' => FILTER_VALIDATE_INT,
 					'z' => FILTER_VALIDATE_INT
@@ -35,46 +35,46 @@ final class ModelFactory
   		  	),
 			'content' => FILTER_SANITIZE_ENCODED
 		)
-	);
+	);*/
     
     
     // $type is the name of mongo collection like users, boards, tickets, events
     public function createModel ($type, $array)
     {
 		// what is this shit??
-        if (isset($model['__v'])) { unset($model['__v']); }
+        // if (isset($model['__v'])) { unset($model['__v']); }
 		
 		// change id fields from mongoId object to string
-        if (isset($array['_id'])) { $array['_id'] = (string)$array['_id']; }
-        if (isset($array['board'])) { $array['board'] = (string)$array['board']; }
-        if (isset($array['createdBy'])) { $array['createdBy'] = (string)$array['createdBy']; }
-        if (isset($array['data']['id'])) { $array['data']['id'] = (string)$array['data']['id']; }
+        // if (isset($array['_id'])) { $array['_id'] = (string)$array['_id']; }
+        // if (isset($array['board'])) { $array['board'] = (string)$array['board']; }
+        // if (isset($array['createdBy'])) { $array['createdBy'] = (string)$array['createdBy']; }
+        // if (isset($array['data']['id'])) { $array['data']['id'] = (string)$array['data']['id']; }
 		
-		$validated = filter_var_array($array, $validatingArray[$type]);
+		$validated = $array; //filter_var_array($array, $validatingArray[$type]);
 		
         // array to stdClass object
         $model = json_decode(json_encode($validated));
         
         
         // what is this shit??
-        //if (isset($model->__v)) { unset($model->__v); } 
+        if (isset($model->__v)) { unset($model->__v); } 
         
         // pretty id fields
-        // if (isset($model->_id)) { $model->_id = $model->_id->{'$id'}; }
-        // if (isset($model->board)) { $model->board = $model->board->{'$id'}; }
-        // if (isset($model->createdBy)) { $model->createdBy = $model->createdBy->{'$id'}; }
-        // if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
+        if (isset($model->_id)) { $model->_id = $model->_id->{'$id'}; }
+        if (isset($model->board)) { $model->board = $model->board->{'$id'}; }
+        if (isset($model->createdBy)) { $model->createdBy = $model->createdBy->{'$id'}; }
+        if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
         
         
         return $model;
         
     }
     
-	private function validate_array($args) {
+	/*private function validate_array($args) {
 	    return function ($data) use ($args) {
 	        return filter_input_array($data, $args);
 	    };
-	}
+	}*/
     
     //not used at this moment
     private function _validateModel ($type, $model)
