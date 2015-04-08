3b631df911bce21e758940f6a1c8d3937677a8b6
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-04-08 12:31:16 +0300

    Modelfactory fixes

--- adminpanel/api/ModelFactory.class.php
+++ adminpanel/api/ModelFactory.class.php
@@ -8,7 +8,7 @@ final class ModelFactory
 	//this is a test, modify values if need be
     private $validatingArray = array(
 		'users' => array(
-			'_id' => FILTER_VALIDATE_INT,
+			'_id' => FILTER_SANITIZE_ENCODED,
 			'email' => FILTER_VALIDATE_EMAIL,
 			'password' => FILTER_SANITIZE_ENCODED,
 			'token' => FILTER_FLAG_NONE,
@@ -18,8 +18,8 @@ final class ModelFactory
 			)
 		),
 		'tickets' => array(
-			'_id' => FILTER_VALIDATE_INT,
-			'board' => FILTER_VALIDATE_INT,
+			'_id' => FILTER_SANITIZE_ENCODED,
+			'board' => FILTER_SANITIZE_ENCODED,
 			'position' => array (
 				'filter' => FILTER_CALLBACK, 		//filter_var_array does not have recursion
 				'options' => validate_array(array(	//this is a workaround
@@ -31,7 +31,7 @@ final class ModelFactory
 			'color' => array( //chech if color format is #FFFFFF
     			'filter' => FILTER_VALIDATE_REGEXP,
     			'flags'     => FILTER_NULL_ON_FAILURE,
-    			'options'   => array('regexp'=>'/^#.[a-f0-9]{6}$/')
+    			'options'   => array('regexp'=>'/^#[[:xdigit:]]{6}$/')
   		  	),
 			'content' => FILTER_SANITIZE_ENCODED
 		)
@@ -41,21 +41,29 @@ final class ModelFactory
     // $type is the name of mongo collection like users, boards, tickets, events
     public function createModel ($type, $array)
     {
+		// what is this shit??
+        if (isset($model['__v'])) { unset($model['__v']); }
 		
+		// change id fields from mongoId object to string
+        if (isset($array['_id'])) { $array['_id'] = (string)$array['_id']; }
+        if (isset($array['board'])) { $array['board'] = (string)$array['board']; }
+        if (isset($array['createdBy'])) { $array['createdBy'] = (string)$array['createdBy']; }
+        if (isset($array['data']['id'])) { $array['data']['id'] = (string)$array['data']['id']; }
+		
+		$validated = filter_var_array($array, $validatingArray[$type]);
 		
         // array to stdClass object
-        $model = json_decode(json_encode(filter_var_array($array, $validatingArray[$type])));
+        $model = json_decode(json_encode($validated));
         
         
         // what is this shit??
-        if (isset($model->__v)) { unset($model->__v); }
-        
+        //if (isset($model->__v)) { unset($model->__v); } 
         
         // pretty id fields
-        if (isset($model->_id)) { $model->_id = $model->_id->{'$id'}; }
-        if (isset($model->board)) { $model->board = $model->board->{'$id'}; }
-        if (isset($model->createdBy)) { $model->createdBy = $model->createdBy->{'$id'}; }
-        if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
+        // if (isset($model->_id)) { $model->_id = $model->_id->{'$id'}; }
+        // if (isset($model->board)) { $model->board = $model->board->{'$id'}; }
+        // if (isset($model->createdBy)) { $model->createdBy = $model->createdBy->{'$id'}; }
+        // if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
         
         
         return $model;
