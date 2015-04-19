3c849a6bb15ddaaf8c33f99595c7bcd6f6a06131
AatuPitkanen <aatu.pitkanen@gmail.com>
2015-04-20 00:59:18 +0300

    Update UserController.js

--- adminpanel/UserController.js
+++ adminpanel/UserController.js
@@ -1,14 +1,17 @@
 var app = angular.module('AppGet', ['ngRoute', 'ngResource']);
 
 app.controller('UserController', [ '$http', '$scope', '$resource', function($http, $scope, $resource) {
-
-           var GetData = this;
-		   $scope.GetData = [];
+       
+		  $scope.GetPutData = [];
+		  $scope.GetUsers = [];
 		   $scope.IdSelected = null;
 		   
   $http.get('http://localhost:8001/api/users/').success(function(data) {
-		   $scope.GetData = data;
-		    }); //http://127.0.0.1:8000/json*/
+		   $scope.GetUsers = data;
+		    });
+  $http.get('http://localhost:8001/api/boards/').success(function(data) {
+		   $scope.GetPutData = data;
+		    });
   
   var logResult = function (str, data, status)
     {
@@ -16,48 +19,26 @@ app.controller('UserController', [ '$http', '$scope', '$resource', function($htt
         "data: " + data + "\n\n" +
         "status: " + status + "\n\n" ;
     };
-  
- $scope.postCall = function() { 
-      console.log($scope.postParam1, $scope.postParam2, $scope.postParam3);
-	
-   var PostParam = {
-        Username: $scope.postParam1,
-        Password: $scope.postParam2,
-		Adminlevel: $scope.postParam3
-	  };    
-    $scope.msg = {Username: $scope.postParam1, Password: $scope.postParam2, Adminlevel: $scope.postParam3};
- 
-    $http.post("http://localhost:8001/api/users/", $scope.msg)
-        .success(function (data, status, headers, config)
-        {
-		 $scope.msg = data;
-         $scope.postCallResult = logResult("POST SUCCESS", data, status, headers);
-        })
-        .error(function (data, status, headers)
-        {
-          $scope.postCallResult = logResult("POST ERROR", data, status, headers);
-        });
-    };
+
 	
  $scope.putCall = function() { 
-    //  console.log($scope.putParam1, $scope.putParam2, $scope.putParam3);
+  
 	
    var putParam = {
-        Username: $scope.putParam1,
-        Password: $scope.putParam2,
-		Adminlevel: $scope.putParam3
+        email: $scope.putParam1,
+        password: $scope.putParam2,
 	  };    
-    $scope.msg = {Username: $scope.putParam1, Password: $scope.putParam2, Adminlevel: $scope.putParam3};
+    console.log($scope.putParam1, $scope.putParam2, $scope.BoardFilter);
  
-    $http.post("http://localhost:8001/api/users/", $scope.msg)
+    $http.put("http://localhost:8001/api/users/"+ $scope.BoardFilter, putParam)
         .success(function (data, status, headers, config)
         {
-		 $scope.msg = data;
-         $scope.postCallResult = logResult("POST SUCCESS", data, status, headers);
+		 putParam = data;
+         $scope.postCallResult = logResult("PUT SUCCESS", data, status, headers);
         })
         .error(function (data, status, headers)
         {
-          $scope.postCallResult = logResult("POST ERROR", data, status, headers);
+          $scope.postCallResult = logResult("PUT ERROR", data, status, headers);
         });
     };
 	
@@ -65,7 +46,7 @@ app.controller('UserController', [ '$http', '$scope', '$resource', function($htt
  $scope.deleteCall = function(IdSelected) {
      $scope.IdSelected = IdSelected;
 
-    $http.delete('http://localhost:8001/api/users/' + IdSelected)
+    $http.delete('http://localhost:8001/api/boards/' + IdSelected)
         .success(function(data) {
             $scope.GetData = data;
             console.log(data);
@@ -75,24 +56,5 @@ app.controller('UserController', [ '$http', '$scope', '$resource', function($htt
             console.log('Error: ' + data);
         });
 };
-$scope.update = function() {
-  $http.get('http://localhost:8001/api/users/').success(function(data) {
-		   $scope.GetData= data;
-		   console.table(data);
-		    }); //http://127.0.0.1:8000/json*/
-}
-
-  /*$scope.deleteCall = function($resource) {
- 
-	    $scope.id = '@id';
-		var DeleteParam = {
-		id: $scope.id
-  		 }
-     	$scope.msg = {id: $Scope.id};
-	
-	return  $resource('http://127.0.0.1:8000/json/users/:id', {id: $scope.id},
-		{'Delete': {method: 'DELETE', params: {id: $scope.msg}} //{Id: '@Id'}
-		});
-		}
-		*/			
+		
  }]);
