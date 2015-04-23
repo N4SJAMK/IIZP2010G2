6cfab419513c5bd9dffa39a0023a70a0e9d24435
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-04-23 19:12:07 +0300

    Modelfactory validating should work

--- adminpanel/api/ModelFactory.class.php
+++ adminpanel/api/ModelFactory.class.php
@@ -6,51 +6,153 @@ namespace api;
 final class ModelFactory
 {
 	//this is a test, modify values if need be
-    /* private $validatingArray = array(
+    private $validatingArray = array(
+		//Validating user-array
 		'users' => array(
-			'_id' => FILTER_SANITIZE_ENCODED,
-			'email' => FILTER_VALIDATE_EMAIL,
-			'password' => FILTER_SANITIZE_ENCODED,
-			'token' => FILTER_FLAG_NONE,
+		
+		
+			'_id' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'email' => array (
+				'filter'  => FILTER_VALIDATE_EMAIL,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+			),
+				
+			'password' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'token' => array (
+				'filter'  => FILTER_UNSAFE_RAW,
+			),
+			
 			'__boards' => array(
 				'filter' => FILTER_VALIDATE_INT,
-				'flags' => FILTER_FORCE_ARRAY
-			)
-		),
-		'tickets' => array(
-			'_id' => FILTER_SANITIZE_ENCODED,
-			'board' => FILTER_SANITIZE_ENCODED,
-			'position' => array (
-				'filter' => FILTER_CALLBACK, 		//filter_var_array does not have recursion this is a workaround
-				'options' => validate_array(array(
-					'x' => FILTER_VALIDATE_INT,
-					'y' => FILTER_VALIDATE_INT,
-					'z' => FILTER_VALIDATE_INT
-				))
+				'flags' => FILTER_FORCE_ARRAY,
+				'options' => array (
+					'default'   => 0,
+				),
 			),
+		),
+		//Validating ticket-array
+		'tickets' => array(
+		
+			'_id' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'board' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			
+			'position' => array (
+				'filter'  => FILTER_VALIDATE_INT,
+				'flags'   => FILTER_REQUIRE_ARRAY,
+				'options' => array (
+					'default'   => array(
+						'Z' => 0,
+						'Y' => 0,
+						'X' => 0,
+					),
+				),
+			),	
+				
 			'color' => array( //chech if color format is #FFFFFF
     			'filter' => FILTER_VALIDATE_REGEXP,
     			'flags'     => FILTER_NULL_ON_FAILURE,
     			'options'   => array('regexp'=>'/^#[[:xdigit:]]{6}$/')
   		  	),
-			'content' => FILTER_SANITIZE_ENCODED
+			
+			'content' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'options' => array (
+					'regexp' => '/.+/',
+					'default'   => '',
+				),
+			),
+		),
+		//Validating board-array
+		'boards' => array(
+			'_id' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'accessCode' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'background' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'options' => array (
+					'regexp' => '/.+/',
+					'default'   => 'none',
+                ),
+            ),
+			'createdBy' => array (
+				'filter'  => FILTER_VALIDATE_REGEXP,
+				'flags'   => FILTER_NULL_ON_FAILURE,
+				'options' => array (
+					'regexp' => '/.+/',
+				),
+			),
+			
+			'name' => FILTER_SANITIZE_ENCODED,
+			'size' => array (
+				'filter'  => FILTER_VALIDATE_INT,
+				'flags'   => FILTER_REQUIRE_ARRAY,
+				'options' => array (
+					'default'   => array(
+						'height' => 0,
+						'width'  => 0,
+					),
+				),
+			),
+			'__tickets'  => array(
+				'filter' => FILTER_VALIDATE_INT,
+				'flags' => FILTER_FORCE_ARRAY
+			)
 		)
-	);*/
+	);
     
     
     // $type is the name of mongo collection like users, boards, tickets, events
     public function createModel ($type, $array)
     {
-		// what is this shit??
-        // if (isset($model['__v'])) { unset($model['__v']); }
 		
 		// change id fields from mongoId object to string
-        // if (isset($array['_id'])) { $array['_id'] = (string)$array['_id']; }
-        // if (isset($array['board'])) { $array['board'] = (string)$array['board']; }
-        // if (isset($array['createdBy'])) { $array['createdBy'] = (string)$array['createdBy']; }
-        // if (isset($array['data']['id'])) { $array['data']['id'] = (string)$array['data']['id']; }
+        if (isset($array['_id'])) { $array['_id'] = $array['_id']->{'$id'}; }
+        if (isset($array['board'])) { $array['board'] = $array['board']->{'$id'}; }
+        if (isset($array['createdBy'])) { $array['createdBy'] = $array['createdBy']->{'$id'}; }
+        if (isset($array['data']['id'])) { $array['data']['id'] = $array['data']['id']->{'$id'}; }
 		
-		$validated = $array; //filter_var_array($array, $validatingArray[$type]);
+		$validated = filter_var_array($array, $this->validatingArray[$type]);
 		
         // array to stdClass object
         $model = json_decode(json_encode($validated));
@@ -59,32 +161,7 @@ final class ModelFactory
         // what is this shit??
         if (isset($model->__v)) { unset($model->__v); } 
         
-        // pretty id fields
-        if (isset($model->_id)) { $model->_id = $model->_id->{'$id'}; }
-        if (isset($model->board)) { $model->board = $model->board->{'$id'}; }
-        if (isset($model->createdBy)) { $model->createdBy = $model->createdBy->{'$id'}; }
-        if (isset($model->data->id)) { $model->data->id = $model->data->id->{'$id'}; }
         
-        
-        return $model;
-        
-    }
-    
-	/*private function validate_array($args) {
-	    return function ($data) use ($args) {
-	        return filter_input_array($data, $args);
-	    };
-	}*/
-    
-    //not used at this moment
-    private function _validateModel ($type, $model)
-    {
-        
-        // validate model properties against model type
-        
-        // if validate not ok
-        // return false
-        // else
         return $model;
         
     }
